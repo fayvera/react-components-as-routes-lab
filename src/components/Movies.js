4bad449c096b7ee5b7418434ac37e83561948680
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-21 15:48:27 -0400

    commit tree 15e89b65fb7c96315fddcb2f75938efb312847d6

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+       {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Title: {movie.title}</h3>
+          Time: {movie.time} <br></br>
+          Genres: {movie.genres}<br></br>
+          Metascore: {movie.metascore}
+        </div>
+       ))}
     </div>
   );
 };
